1e191c92dbc8599e52b9be8acff0978b73a01614
ProgrammerFang <alfa24ae@student.cbs.dk>
2025-12-05 14:39:12 +0100

    Update deepseek.js

--- MinDisApp2025/routes/deepseek.js
+++ MinDisApp2025/routes/deepseek.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
-const chatController = require('../controllers/deepseekController');
+const chatController = require('../controllers/deepseekcontroller');
+
 
 // Definer endpoints
 router.post('/chat', chatController.handleChat);
