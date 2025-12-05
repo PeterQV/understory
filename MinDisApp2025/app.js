c3dae53b961314b673126bf0202ae519da1b8883
ProgrammerFang <alfa24ae@student.cbs.dk>
2025-12-05 15:17:32 +0100

    Update app.js

--- MinDisApp2025/app.js
+++ MinDisApp2025/app.js
@@ -43,6 +43,9 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'public', 'index.html'));
 });
 
+app.get('/auth/login', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'login.html'));
+});
 
 app.get('/health', (req, res) => {
   res.json({ 
